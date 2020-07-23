141c0c0c2a490c7bf9f09d560f827d31e65a6d12
arevello <alexander.revello@maine.edu>
2020-07-23 19:06:17 -0400

    primitive mask

---
+++ BirdCNN/imageUtils.py
@@ -0,0 +1,153 @@
+'''
+Created on Jul 17, 2020
+
+@author: Alex
+'''
+
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+from osgeo import gdal
+from osgeo.gdalconst import *
+
+import mathUtils
+import birdConstants
+
+class ImageUtilities(object):
+    '''
+    classdocs
+    '''
+    mu = None
+    bc = None
+    dumpFile = []
+    
+    def parseImages(self, tifFiles, tifIdx, csvFiles, fileContents):
+        gdal.AllRegister()
+
+        #appleIslandFile = 'D:\Appledore\AppledoreIsland_SmuttynoseIsland_CedarIsland_81191_81182_81194_20190531_Ortho_Multi_2CM.tif'
+        
+        csvIdx = 0
+        for t in range(len(tifFiles)):
+            
+            fh = gdal.Open(tifFiles[t], GA_ReadOnly)
+            
+            while tifIdx[csvIdx] < t:
+                csvIdx += 1
+            
+            self.dumpFile = []
+            
+            while tifIdx[csvIdx] == t:
+                
+                tifList = []
+                print(t, tifFiles[tifIdx[csvIdx]], csvFiles[csvIdx])
+                
+                if fh is None:
+                    print("failed to open")
+                    exit(1)
+                
+                #for i in range(len(test)):
+                    #print(i)
+                    #print(test[i][13], test[i][14])
+                    #print(test[i])
+                
+                cols = fh.RasterXSize
+                rows = fh.RasterYSize
+                bands = fh.RasterCount
+                transform = fh.GetGeoTransform()
+                xOrig = transform[0]
+                yOrig = transform[3]
+                pixelWidth = transform[1]
+                pixelHeight = transform[5]
+                
+                for f in range(len(fileContents[csvIdx])):
+                    csvList = []
+                    #dumpFile.append([])
+                    #print(fileContents[csvIdx][f].get('POINT_X'))
+                    
+                    #format: [tifFile][csvFile][species, behavior, [data]]
+                    try:
+                        xOff, yOff = self.mu.getPixelCoords(float(fileContents[csvIdx][f].get('POINT_X')), float(fileContents[csvIdx][f].get('POINT_Y')), xOrig, yOrig, pixelWidth, pixelHeight)
+                        species = self.bc.strToSpecies(fileContents[csvIdx][f].get('Species'))
+                        behavior = int(fileContents[csvIdx][f].get('Behavior'))
+                        csvList.append(species)
+                        csvList.append(behavior)
+                    
+                        for i in range(fh.RasterCount):
+                            #csvList.append(i)
+                            bands2 = fh.GetRasterBand(i+1)
+                            #print(bands2.XSize, bands2.YSize)
+                            #print(i)
+        
+                            data = bands2.ReadAsArray(xOff-15, yOff-15, 30, 30)
+                            csvList.append(data)
+                            plt.imshow(data)
+                            plt.show()
+                    except Exception as e:
+                        print(e)
+                        print("issue with file", csvFiles[csvIdx])
+                        break
+                    
+                    tifList.append(csvList)
+                
+                csvIdx += 1
+                if csvIdx >= len(tifIdx):
+                    break
+            
+            gdal.Unlink(tifFiles[t])
+            self.dumpFile.append(tifList)
+
+    def dumpFile(self, fileName):
+        with open(fileName, 'wb') as outfile:
+            pickle.dump(self.dumpFile, outfile, pickle.HIGHEST_PROTOCOL)
+            
+    def openDump(self, fileName):
+        with open(fileName, 'rb') as infile:
+            loadFile = pickle.load(infile)
+            return loadFile
+        
+    def generateMask(self, infile, closeGap=False):
+        print("temp")
+        
+        #print(infile[0][2])
+        
+        data = infile[0][0][2]
+        mask = np.zeros((30,30))
+        
+        #start 14,15
+        rowCount = 2
+        start = 14
+        stop = 15
+        firstIter = True
+        thresh = 100
+        while start >= 0 and stop <= 29:
+            for i in range(start, stop+1):
+                for j in range(start, stop+1):
+                    if firstIter or data[i][j] >= thresh:
+                        mask[i][j] = 1
+            firstIter = False
+            start -= 1
+            stop += 1
+            
+        plt.imshow(data)
+        plt.show()
+        
+        plt.imshow(mask)
+        plt.show()
+        #while not at edge and new additions made
+        #get surrounding ring of prev center
+        #if above thresh and at least 1 mask neighbor set to 1
+        #else 0
+        
+        #for each pixel
+        #if has >= thresh positive set to 1
+        
+        #edge detection????
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        mu = mathUtils.MathUtilities()
+        bc = birdConstants.BirdConstants()
+        
--- BirdCNN/main.py
+++ BirdCNN/main.py
@@ -4,18 +4,11 @@ Created on Jun 8, 2020
 @author: Alex
 '''
 
-import birdConstants
 import fileUtils
-import mathUtils
-import matplotlib.pyplot as plt
-import pickle
-
-from osgeo import gdal
-from osgeo.gdalconst import *
+import imageUtils
 
 fu = fileUtils.FileUtilities()
-mu = mathUtils.MathUtilities()
-bc = birdConstants.BirdConstants()
+iu = imageUtils.ImageUtilities()
 
 #fu.removeCsvFiles('D:\\')
 
@@ -26,9 +19,6 @@ bc = birdConstants.BirdConstants()
 csvFiles,tifIdx = fu.searchFiles('D:\\', ".csv", pairWithTif=True)
 tifFiles = fu.searchFiles('D:\\', ".tif")
 
-print(csvFiles)
-print(tifIdx)
-
 #13 x
 #14 y
 # AppledoreIsland_SmuttynoseIsland_CedarIsland_81191_81182_81194_20190531_Ortho_Multi_2CM.tif
@@ -37,79 +27,8 @@ fileContents = []
 for i in range(len(csvFiles)):
     fileContents.append(fu.openCsvFile(csvFiles[i]))
 
+#iu.parseImages(tifFiles, tifIdx, csvFiles, fileContents)
+#iu.dumpFile('D:\\trainImg.pkl')
 
-gdal.AllRegister()
-
-#appleIslandFile = 'D:\Appledore\AppledoreIsland_SmuttynoseIsland_CedarIsland_81191_81182_81194_20190531_Ortho_Multi_2CM.tif'
-
-csvIdx = 0
-for t in range(len(tifFiles)):
-    
-    fh = gdal.Open(tifFiles[t], GA_ReadOnly)
-    
-    while tifIdx[csvIdx] < t:
-        csvIdx += 1
-    
-    dumpFile = []
-    
-    while tifIdx[csvIdx] == t:
-        
-        tifList = []
-        print(t, tifFiles[tifIdx[csvIdx]], csvFiles[csvIdx])
-        
-        if fh is None:
-            print("failed to open")
-            exit(1)
-        
-        #for i in range(len(test)):
-            #print(i)
-            #print(test[i][13], test[i][14])
-            #print(test[i])
-        
-        cols = fh.RasterXSize
-        rows = fh.RasterYSize
-        bands = fh.RasterCount
-        transform = fh.GetGeoTransform()
-        xOrig = transform[0]
-        yOrig = transform[3]
-        pixelWidth = transform[1]
-        pixelHeight = transform[5]
-        
-        for f in range(len(fileContents[csvIdx])):
-            csvList = []
-            #dumpFile.append([])
-            #print(fileContents[csvIdx][f].get('POINT_X'))
-            
-            try:
-                xOff, yOff = mu.getPixelCoords(float(fileContents[csvIdx][f].get('POINT_X')), float(fileContents[csvIdx][f].get('POINT_Y')), xOrig, yOrig, pixelWidth, pixelHeight)
-                species = bc.strToSpecies(fileContents[csvIdx][f].get('Species'))
-                behavior = int(fileContents[csvIdx][f].get('Behavior'))
-                csvList.append(species)
-                csvList.append(behavior)
-            
-                for i in range(fh.RasterCount):
-                    #csvList.append(i)
-                    bands2 = fh.GetRasterBand(i+1)
-                    #print(bands2.XSize, bands2.YSize)
-                    #print(i)
-
-                    data = bands2.ReadAsArray(xOff-15, yOff-15, 30, 30)
-                    csvList.append(data)
-                    #plt.imshow(data)
-                    #plt.show()
-            except Exception as e:
-                print(e)
-                print("issue with file", csvFiles[csvIdx])
-                break
-            
-            tifList.append(csvList)
-        
-        csvIdx += 1
-        if csvIdx >= len(tifIdx):
-            break
-    
-    gdal.Unlink(tifFiles[t])
-    dumpFile.append(tifList)
-    
-with open('D:\\trainImg.pkl', 'wb') as outfile:
-    pickle.dump(dumpFile, outfile, pickle.HIGHEST_PROTOCOL)
+trainData = iu.openDump('D:\\trainImg.pkl')
+iu.generateMask(trainData)
